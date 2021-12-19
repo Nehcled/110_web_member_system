d22f52d03f828b9b5118188d3e7ac864f6e57c66
D0948083 <D0948083@o365.fcu.edu.tw>
2021-12-19 16:00:46 +0800

    Increase some columns.

--- web_member_system/member/models.py
+++ web_member_system/member/models.py
@@ -1,6 +1,8 @@
-from django.db import models
 from django.contrib.auth.models import User,AbstractUser
+from django.db import models
 from django.urls import reverse
+
+from datetime import datetime, timezone
 import uuid
 
 # Create your models here
@@ -14,8 +16,11 @@ class UserProfile(models.Model):
     cel = models.CharField(max_length=100, null=True)
     tel = models.CharField(max_length=100, null=True)
 
+    head_shot = models.CharField(max_length=100, default="template_of_head_shot.png")
     member_level = models.DecimalField(max_digits=10, decimal_places = 0, default=1)
     member_exp = models.DecimalField(max_digits=10, decimal_places = 0, default=0)
+
+    last_login_time = models.DateField(null=True)
     # profile_photo = models.ImageField(upload_to='',null=True)
 
     
